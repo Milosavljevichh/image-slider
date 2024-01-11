492025589ac09b4ef2d0ec2f1a4820f1f62f6d50
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-01-11 17:14:20 +0100

    fixed double addition bug which caused multiple images to show

--- slider.js
+++ slider.js
@@ -7,6 +7,8 @@ let previous = document.getElementById('previous');
 
 let dots_container = document.getElementById('dotsContainer');
 
+let readyToShowSlide = true;
+
 //add dots below the slider frame, depending on how many slides there are
 for (let i=0;i<slides.length;i++){
     let dot = document.createElement('img');
@@ -33,70 +35,80 @@ if (result) {
 };
 
 function loadNextSlide() {
-    active++
-    //if the active number exceeds the number of slides,
-    //set the first slide to active and give it a fade-in animation
-    //on animation end, set active to 0 and disabled the last slide of the slides array
-    //remove the event listener at the end in order to not cause bugs
-    if (active === slides.length) {
-        slides[0].classList.toggle('inactive');
-        dots_array[0].src = 'img/button.png';
-        dots_array[dots_array.length - 1].src = 'img/circle.png';
-        slides[0].classList.add('fade-in');
+    if (readyToShowSlide) {
+        readyToShowSlide = false;
         next.disabled = true;
-        slides[0].addEventListener('animationend', function nextSlide(){
-            active = 0;
-            slides[slides.length - 1].classList.toggle('inactive');
-            slides[0].classList.remove('fade-in');
-            slides[0].removeEventListener('animationend', nextSlide);
-            next.disabled = false;
+        active++
+        //if the active number exceeds the number of slides,
+        //set the first slide to active and give it a fade-in animation
+        //on animation end, set active to 0 and disabled the last slide of the slides array
+        //remove the event listener at the end in order to not cause bugs
+        if (active === slides.length) {
+            slides[0].classList.toggle('inactive');
+            dots_array[0].src = 'img/button.png';
+            dots_array[dots_array.length - 1].src = 'img/circle.png';
+            slides[0].classList.add('fade-in');
+            slides[0].addEventListener('animationend', function nextSlide(){
+                active = 0;
+                slides[slides.length - 1].classList.toggle('inactive');
+                slides[0].classList.remove('fade-in');
+                slides[0].removeEventListener('animationend', nextSlide);
+                next.disabled = false;
+                readyToShowSlide = true;
+                loopThroughSlides();
+            });
+        //if the active number is in range of the number of slides,
+        //show the next slide, give it an animation and when animation ends,
+        //set the previous slide to inactive
+        } else {
+            slides[active].classList.toggle('inactive');
+            dots_array[active].src = 'img/button.png';
+            dots_array[active - 1].src = 'img/circle.png';
+            slides[active].classList.add('fade-in');
+            slides[active].addEventListener('animationend', function nextSlide(){
+                slides[active-1].classList.toggle('inactive');
+                slides[active].classList.remove('fade-in');
+                slides[active].removeEventListener('animationend', nextSlide);
+                next.disabled = false;
+                readyToShowSlide = true;
+                loopThroughSlides();
         });
-    //if the active number is in range of the number of slides,
-    //show the next slide, give it an animation and when animation ends,
-    //set the previous slide to inactive
-    } else {
-        slides[active].classList.toggle('inactive');
-        dots_array[active].src = 'img/button.png';
-        dots_array[active - 1].src = 'img/circle.png';
-        slides[active].classList.add('fade-in');
-        next.disabled = true;
-        slides[active].addEventListener('animationend', function nextSlide(){
-            slides[active-1].classList.toggle('inactive');
-            slides[active].classList.remove('fade-in');
-            slides[active].removeEventListener('animationend', nextSlide);
-            next.disabled = false;
-        });
-    }
+    }}
 };
 
 function loadPreviousSlide() {
-    active--
-    if (active === -1) {
-        slides[slides.length - 1].classList.toggle('inactive');
-        dots_array[dots_array.length - 1].src = 'img/button.png';
-        dots_array[0].src = 'img/circle.png';
-        slides[slides.length - 1].classList.add('fade-in');
+    if (readyToShowSlide) {
+        readyToShowSlide = false;
         previous.disabled = true;
-        slides[slides.length - 1].addEventListener('animationend', function nextSlide(){
-            active = slides.length - 1;
-            slides[0].classList.toggle('inactive');
-            slides[slides.length - 1].classList.remove('fade-in');
-            slides[slides.length - 1].removeEventListener('animationend', nextSlide);
-            previous.disabled = false;
-        })
-    } else {
-        slides[active].classList.toggle('inactive');
-        dots_array[active].src = 'img/button.png';
-        dots_array[active+1].src = 'img/circle.png';
-        slides[active].classList.add('fade-in');
-        previous.disabled = true;
-        slides[active].addEventListener('animationend', function nextSlide(){
-            slides[active+1].classList.toggle('inactive');
-            slides[active].classList.remove('fade-in');
-            slides[active].removeEventListener('animationend', nextSlide);
-            previous.disabled = false;
-        });
-    }
+        active--
+        if (active === -1) {
+            slides[slides.length - 1].classList.toggle('inactive');
+            dots_array[dots_array.length - 1].src = 'img/button.png';
+            dots_array[0].src = 'img/circle.png';
+            slides[slides.length - 1].classList.add('fade-in');
+            slides[slides.length - 1].addEventListener('animationend', function nextSlide(){
+                active = slides.length - 1;
+                slides[0].classList.toggle('inactive');
+                slides[slides.length - 1].classList.remove('fade-in');
+                slides[slides.length - 1].removeEventListener('animationend', nextSlide);
+                previous.disabled = false;
+                readyToShowSlide = true;
+                loopThroughSlides();
+            })
+        } else {
+            slides[active].classList.toggle('inactive');
+            dots_array[active].src = 'img/button.png';
+            dots_array[active+1].src = 'img/circle.png';
+            slides[active].classList.add('fade-in');
+            slides[active].addEventListener('animationend', function nextSlide(){
+                slides[active+1].classList.toggle('inactive');
+                slides[active].classList.remove('fade-in');
+                slides[active].removeEventListener('animationend', nextSlide);
+                previous.disabled = false;
+                readyToShowSlide = true;
+                loopThroughSlides();
+            });
+    }}
 };
 
 next.addEventListener('click', loadNextSlide);
@@ -105,8 +117,7 @@ previous.addEventListener('click', loadPreviousSlide);
 function loopThroughSlides() {
     setTimeout(() => {
         loadNextSlide();
-        loopThroughSlides();
-    }, 3500);
+    }, 700);
 };
 
 loopThroughSlides();
